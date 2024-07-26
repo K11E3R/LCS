2067551ac152cd8fe5ccf5fb8adb4183b1e17a79
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:07 +0200

    Solution for .:

---
+++ pass-the-pillow/Accepted/7-10-2024, 6:24:55 PM/Solution.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/pass-the-pillow
+
+impl Solution {
+    pub fn pass_the_pillow(n: i32, time: i32) -> i32 {
+        let mut k = time / (n - 1);
+        let mut _mod = time % (n - 1);
+
+        if (k & 1) == 1 {
+            return n - _mod;
+        }
+
+        _mod + 1
+    }
+}
