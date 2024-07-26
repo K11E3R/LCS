c8e1eebbb79dce01f3b15038b3c3464ca9c7e0f2
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:59 +0200

    Solution for .:

---
+++ find-all-numbers-disappeared-in-an-array/Time Limit Exceeded/7-10-2024, 11:49:54 PM/Solution.py
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array
+
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        return [i for i in range(1, len(nums)+1) if i not in nums]
