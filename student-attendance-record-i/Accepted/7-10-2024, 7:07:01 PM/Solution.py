a997f66d7eb4d733363b65713894279c7ea1d1e8
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:12 +0200

    Solution for .:

---
+++ student-attendance-record-i/Accepted/7-10-2024, 7:07:01 PM/Solution.py
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/student-attendance-record-i
+
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        if sum([1 for i in s if "A" is i or "LLL" in s])>1:
+            return False
+        return True
