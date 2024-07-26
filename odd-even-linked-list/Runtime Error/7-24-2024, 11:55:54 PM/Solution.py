ef89ea696aa843b220a8bb0ce9c164fedf84179e
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:07 +0200

    Solution for .:

---
+++ odd-even-linked-list/Runtime Error/7-24-2024, 11:55:54 PM/Solution.py
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/odd-even-linked-list
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head):
+        if not head : return head
+        odd, evn, dum = head, head.next, head.nex
+        while evn and evn.next:
+            odd.next, evn.next = odd, evn = odd.next.next, evn.next.next
+        odd.next = dum
+        return head
