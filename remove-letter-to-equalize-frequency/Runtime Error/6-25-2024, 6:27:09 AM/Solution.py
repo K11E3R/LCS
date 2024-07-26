c55a58603ab1820c41ac0094e15fef86a3b7b6b0
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:09 +0200

    Solution for .:

---
+++ remove-letter-to-equalize-frequency/Runtime Error/6-25-2024, 6:27:09 AM/Solution.py
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency
+
+# class Solution(object):
+#     def equalFrequency(self, word):
+#         return "true" if len(set(word))+1 == len(word) else "false"
