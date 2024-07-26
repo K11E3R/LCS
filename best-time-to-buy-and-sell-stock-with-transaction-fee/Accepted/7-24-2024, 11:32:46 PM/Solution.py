c77661470d0c5ad46281038317cd1c28e787d053
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:56 +0200

    Solution for .:

---
+++ best-time-to-buy-and-sell-stock-with-transaction-fee/Accepted/7-24-2024, 11:32:46 PM/Solution.py
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        buy = float('-inf')
+        sell = 0 
+        for price in prices:
+            buy = max(buy, sell - price)
+            sell = max(sell, buy + price - fee)
+
+        return sell
