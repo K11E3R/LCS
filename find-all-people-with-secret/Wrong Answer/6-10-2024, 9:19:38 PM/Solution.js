4ba181333dde8856ad78267dbb7fafc096585237
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:00 +0200

    Solution for .:

---
+++ find-all-people-with-secret/Wrong Answer/6-10-2024, 9:19:38 PM/Solution.js
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/find-all-people-with-secret
+
+/**
+ * @param {number} n
+ * @param {number[][]} meetings
+ * @param {number} firstPerson
+ * @return {number[]}
+ */
+var findAllPeople = function(n, meetings, firstPerson) {
+    let res = new Array()
+    res.push(0)
+    res.push(firstPerson)
+    let sortedM = meetings.sort((a,b)=>a[2]-b[2])
+    let List_Meeting = new Array()
+    sortedM.forEach((a)=>{
+        List_Meeting.push([[a[0],a[2]],[a[1],a[2]]])
+    })
+  List_Meeting.forEach((a)=>{
+    if (res.includes(a[0][0]) ){
+        res.push(a[1][0])
+    }
+    if (res.includes(a[1][0]) ){
+        res.push(a[0][0])
+    }
+})
+    return Array.from(new Set(res)).sort((a, b)=> a-b)
+};
