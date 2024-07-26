daf2b0f4bc43c2c84e86a5ca0abf51b346684161
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:10 +0200

    Solution for .:

---
+++ reverse-bits/Accepted/7-13-2024, 10:47:04 PM/Solution.py
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/reverse-bits
+
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            res |= (n & 1) << (31 - i)
+            n >>= 1
+        return res
