15c7b6235d1fb92fd263f0dc625e75a525fe37c2
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:09 +0200

    Solution for .:

---
+++ remove-letter-to-equalize-frequency/Accepted/6-25-2024, 6:36:41 AM/Solution.py
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/remove-letter-to-equalize-frequency
+
+
+class Solution(object):
+    def equalFrequency(self, word):
+        from collections import Counter
+                
+        if len(Counter(list(Counter(word).values()))) == 1:
+            return len(list(Counter(word).values())) == 1 or 1 in list(Counter(word).values())
+        
+        if len(Counter(list(Counter(word).values()))) == 2:
+            (freq1, count1), (freq2, count2) = Counter(list(Counter(word).values())).items()
+            if freq1 > freq2:
+                freq1, freq2 = freq2, freq1
+                count1, count2 = count2, count1
+            return (freq1 == 1 and count1 == 1) or (freq2 == freq1 + 1 and count2 == 1)
+        
+        return False
+
+# class Solution(object):
+#     def equalFrequency(self, word):
+#         return 'true' if len(word) == len(set(word))+1 else 'false'
