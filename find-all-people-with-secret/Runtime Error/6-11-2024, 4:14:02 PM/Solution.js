88a7d863340ae4e8590829e089b5aca5b8fadbe9
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:00 +0200

    Solution for .:

---
+++ find-all-people-with-secret/Runtime Error/6-11-2024, 4:14:02 PM/Solution.js
@@ -0,0 +1,40 @@
+// https://leetcode.com/problems/find-all-people-with-secret
+
+
+var groupMeetingsByIndex = (sortedM) => {
+  return Object.groupBy(sortedM, (a) => a[2] )
+}
+
+var findAllPeople = (n, meetings, firstPerson)=>{
+    let res = new Array()
+    res.push(0)
+    res.push(firstPerson)
+    let sortedM = meetings
+    sortedM.sort((a, b) => a-b)
+    console.debug("-----------------------------------------")
+    console.debug("sorted M: ")
+    console.table(sortedM)
+    var groupbY  = groupMeetingsByIndex(sortedM)
+    console.debug("------------ groupbY ------------")
+    console.table(groupbY)
+    for (var prop in groupbY) {
+      groupbY[prop].sort((a,b) => {
+          if(res.includes(a[0]) || res.includes(a[1])){
+            return -1
+          }
+        })
+
+      groupbY[prop].forEach((a)=>{
+          if (res.includes(a[0]) ){
+                  res.push(a[1])
+              }
+              if (res.includes(a[1]) ){
+                  res.push(a[0])
+              }
+          })
+      console.warn("key : ", prop, "val A: ", groupbY[prop])
+    }
+
+    console.debug("result : ", res)
+    return Array.from(new Set(res)).sort((a, b)=> a-b)
+};
