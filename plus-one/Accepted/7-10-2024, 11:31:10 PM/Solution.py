72323c7ffb4a757cd17fed40365396bc8734f495
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:07 +0200

    Solution for .:

---
+++ plus-one/Accepted/7-10-2024, 11:31:10 PM/Solution.py
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/plus-one
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        a = int(''.join(map(str, digits))) + 1
+        return list(map(int, str(a)))
