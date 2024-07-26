46c92f40993459b83d805d6c2bac1bda86ddd993
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:04 +0200

    Solution for .:

---
+++ length-of-last-word/Accepted/7-12-2024, 11:36:44 PM/Solution.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/length-of-last-word
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        let s = s.trim_end();
+        let n = s.len();
+        for (i, c) in s.char_indices().rev() {
+            if c == ' ' {
+                return (n - i - 1) as i32;
+            }
+        }
+        n as i32
+    }
+}
