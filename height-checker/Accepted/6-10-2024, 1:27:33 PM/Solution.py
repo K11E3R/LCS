2d3db766397a1a0322e9d11eb977b7e4d109bb5d
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:03 +0200

    Solution for .:

---
+++ height-checker/Accepted/6-10-2024, 1:27:33 PM/Solution.py
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/height-checker
+
+class Solution:
+    # def heightChecker(self, heights):
+    #     a = heights.copy();a.sort()
+    #     return sum([ 1 for i in range(len(heights)) if a[i] != heights[i]])
+    def heightChecker(self, heights: List[int]) -> int:
+        c = 0
+        a = [i for i in heights]; a.sort()
+        for i in range(len(heights)):
+            if a[i] != heights[i]:
+                c+=1
+        return c
