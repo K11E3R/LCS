31077a3dbd7046d40e863197fa0fd36333e39abf
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:58 +0200

    Solution for .:

---
+++ decremental-string-concatenation/Accepted/7-10-2024, 9:45:06 PM/Solution.py
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/decremental-string-concatenation
+
+from functools import cache
+from typing import List
+
+class Solution:
+    def minimizeConcatenatedLength(self, words: List[str]) -> int:
+        @cache
+        def dfs(i: int, a: str, b: str) -> int:
+            if i >= len(words):
+                return 0
+            s = words[i]
+            x = dfs(i + 1, a, s[-1]) - (s[0] == b)
+            y = dfs(i + 1, s[0], b) - (s[-1] == a)
+            return len(s) + min(x, y)
+
+        return len(words[0]) + dfs(1, words[0][0], words[0][-1])
+
