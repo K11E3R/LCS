756b9325f36b375b331825f90658fd8dc050a5e1
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:01 +0200

    Solution for .:

---
+++ find-the-encrypted-string/Wrong Answer/7-11-2024, 3:53:56 PM/Solution.py
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/find-the-encrypted-string
+
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        n = len(s)
+        k = k % n  
+        return s[-k:] + s[:-k]
