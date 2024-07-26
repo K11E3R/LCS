b7fe756d0b7adaaeba95dc6b641bd29bb965b911
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:04 +0200

    Solution for .:

---
+++ intersection-of-two-arrays-ii/Accepted/7-19-2024, 12:41:47 AM/Solution.py
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        return list((Counter(nums1) & Counter(nums2)).elements())
