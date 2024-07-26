b3da993558a0149e32c4a961d328393a0161904a
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:08 +0200

    Solution for .:

---
+++ ransom-note/Accepted/7-14-2024, 11:43:39 PM/Solution.py
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/ransom-note
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        cnt = Counter(magazine)
+        for c in ransomNote:
+            cnt[c] -= 1
+            if cnt[c] < 0:
+                return False
+        return True
