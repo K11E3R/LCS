88262eb15114fec79b2e9819ce67c9b6199978bd
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:59 +0200

    Solution for .:

---
+++ final-value-of-variable-after-performing-operations/Accepted/6-19-2024, 7:27:46 PM/Solution.py
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations
+
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        
+        a = {"+": 1, "-":-1}
+        e = [1 if '+' in i else -1 for i in operations]
+        return sum(e)
