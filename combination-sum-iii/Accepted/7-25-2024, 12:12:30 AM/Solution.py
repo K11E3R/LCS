7a84573e65be14b5a9f4cddc0a5bf74e15d66876
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:57 +0200

    Solution for .:

---
+++ combination-sum-iii/Accepted/7-25-2024, 12:12:30 AM/Solution.py
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/combination-sum-iii
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        return [
+            x
+            for x in [
+                reduce(lambda x, y: x + y, s)
+                for s in product(*[[[], [i]] for i in range(1, 10)])
+            ]
+            if len(x) == k and sum(x) == n
+        ]
