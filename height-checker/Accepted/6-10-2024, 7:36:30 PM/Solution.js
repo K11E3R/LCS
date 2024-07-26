e1f7edba3d831e1478b4bd9620e0c1b2b46d3266
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:03 +0200

    Solution for .:

---
+++ height-checker/Accepted/6-10-2024, 7:36:30 PM/Solution.js
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/height-checker
+
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    let s = [...heights]
+    s.sort((a,b)=>a-b)
+    let c = 0
+    // s.forEach( (elem, i ) => {
+    //     if (heights[i] !== elem){
+    //         c++
+    //     }
+    // }
+    // )
+    for(let i=0; i<heights.length;i++){
+        if (heights[i] !== s[i]){
+            c++
+        }
+    }
+    return c
+};
