032345997b8487b4949d448cde864bd9488f5969
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:13 +0200

    Solution for .:

---
+++ three-consecutive-odds/Accepted/7-11-2024, 4:11:18 PM/Solution.py
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/three-consecutive-odds
+
+class Solution:
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        return any(arr[i] % 2 and arr[i + 1] % 2 and arr[i + 2] % 2 for i in range(len(arr) - 2))
