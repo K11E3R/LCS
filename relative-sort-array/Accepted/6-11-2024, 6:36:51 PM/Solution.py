99761fbb5567e85bf86a9db0be5668cded5ec024
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:09 +0200

    Solution for .:

---
+++ relative-sort-array/Accepted/6-11-2024, 6:36:51 PM/Solution.py
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/relative-sort-array
+
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        om = {nn: i for i, nn in enumerate(arr2)}
+        ommm = sorted([nn for nn in arr1 if nn in om], key=lambda x: om[x]) + sorted(nn for nn in arr1 if nn not in om)
+        return ommm   
