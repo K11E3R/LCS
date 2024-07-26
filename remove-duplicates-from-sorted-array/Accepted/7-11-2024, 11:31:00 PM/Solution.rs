6e31cc653217f25475f4d8803ff9ec228bceee99
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:09 +0200

    Solution for .:

---
+++ remove-duplicates-from-sorted-array/Accepted/7-11-2024, 11:31:00 PM/Solution.rs
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut k = 0;
+        for i in 0..nums.len() {
+            if k == 0 || nums[i] != nums[k - 1] {
+                nums[k] = nums[i];
+                k += 1;
+            }
+        }
+        k as i32
+    }
+}
