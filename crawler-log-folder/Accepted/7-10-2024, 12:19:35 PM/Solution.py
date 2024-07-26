d813fb6b1d4aa0070f4ac1f257acc263253eb88f
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:22:57 +0200

    Solution for .:

---
+++ crawler-log-folder/Accepted/7-10-2024, 12:19:35 PM/Solution.py
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/crawler-log-folder
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        count = 0
+        for log in logs:
+            if log == "../":
+                if count > 0:
+                    count -= 1
+            elif log != "./":
+                count += 1
+        return count
