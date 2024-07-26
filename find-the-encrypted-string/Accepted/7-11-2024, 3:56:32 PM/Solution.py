354c56a525d48b7028c3fad3fa677313bf6e8ec5
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:01 +0200

    Solution for .:

---
+++ find-the-encrypted-string/Accepted/7-11-2024, 3:56:32 PM/Solution.py
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/find-the-encrypted-string
+
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        cs = list(s)
+        n = len(s)
+        for i in range(n):
+            cs[i] = s[(i + k) % n]
+        return "".join(cs)
