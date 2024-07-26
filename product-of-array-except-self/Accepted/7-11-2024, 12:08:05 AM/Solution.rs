eb9f3f37aa3a5a5ce16ef3a2f66f8f3b7160757d
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:08 +0200

    Solution for .:

---
+++ product-of-array-except-self/Accepted/7-11-2024, 12:08:05 AM/Solution.rs
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/product-of-array-except-self
+
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let zero_count = nums.iter().filter(|&x| *x == 0).count();
+
+        if zero_count > 1 {
+            return vec![0; nums.len()];
+        }
+
+        // WIIIII
+        let total_product = nums.iter().filter(|&x| *x != 0).product::<i32>();
+        nums.into_iter().map(|num| {
+            if num == 0 {
+                total_product
+            } else if zero_count == 1 {
+                0
+            } else {
+                total_product / num
+            }
+        }).collect::<Vec<i32>>()
+    }
+
+}
