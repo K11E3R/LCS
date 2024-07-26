4fab25f033227bf09b1d8b40bbc7e85c10f7d123
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:00 +0200

    Solution for .:

---
+++ find-all-people-with-secret/Wrong Answer/6-11-2024, 3:41:13 PM/Solution.js
@@ -0,0 +1,57 @@
+// https://leetcode.com/problems/find-all-people-with-secret
+
+function groupMeetingsByIndex(sortedM) {
+  const groupedByIndex = [];
+  sortedM.forEach((a, i)=> {
+    groupedByIndex.push([a[0],a[1],a[2]])
+  })
+  const gg = Object.groupBy(groupedByIndex, (a) => a[2] )
+  return gg;
+}
+const findAllPeople = (n, meetings, firstPerson) => {
+    let res = new Set();
+    res.add(0);
+    res.add(firstPerson);
+
+    // Sort meetings by time
+    meetings.sort((a, b) => a[2] - b[2]);
+
+    // Group meetings by time
+    const groupMeetingsByIndex = (sortedM) => {
+        const groupedByIndex = {};
+        sortedM.forEach((meeting) => {
+            const time = meeting[2];
+            if (!groupedByIndex[time]) {
+                groupedByIndex[time] = [];
+            }
+            groupedByIndex[time].push(meeting);
+        });
+        return groupedByIndex;
+    };
+
+    const groupbY = groupMeetingsByIndex(meetings);
+
+    // Process each time group
+    for (let time in groupbY) {
+        let currentMeetings = groupbY[time];
+        let peopleWithInfo = new Set();
+
+        currentMeetings.forEach(([a, b, t]) => {
+            if (res.has(a) || res.has(b)) {
+                peopleWithInfo.add(a);
+                peopleWithInfo.add(b);
+            }
+        });
+
+        currentMeetings.forEach(([a, b, t]) => {
+            if (peopleWithInfo.has(a)) {
+                res.add(b);
+            }
+            if (peopleWithInfo.has(b)) {
+                res.add(a);
+            }
+        });
+    }
+
+    return Array.from(res).sort((a, b) => a - b);
+};
