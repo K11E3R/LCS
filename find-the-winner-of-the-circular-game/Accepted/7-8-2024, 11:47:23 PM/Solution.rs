92999551b0e93ea08aafc0f91410a093e6552a8d
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:01 +0200

    Solution for .:

---
+++ find-the-winner-of-the-circular-game/Accepted/7-8-2024, 11:47:23 PM/Solution.rs
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/find-the-winner-of-the-circular-game
+
+impl Solution {
+    pub fn find_the_winner(n: i32, k: i32) -> i32 {
+        if n == 1 {
+            return 1;
+        }
+		let mut ans = (k + Solution::find_the_winner(n - 1, k)) % n;
+		return if ans == 0 { n } else { ans };
+    }
+}
