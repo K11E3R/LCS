03e6336faa63dec4b3338cf7694cc8b78b189ccf
Yassine Naanani <27584700+K11E3R@users.noreply.github.com>
2024-07-27 00:23:00 +0200

    Solution for .:

---
+++ find-all-people-with-secret/Wrong Answer/6-10-2024, 7:54:41 PM/Solution.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/find-all-people-with-secret
+
+/**
+ * @param {number} n
+ * @param {number[][]} meetings
+ * @param {number} firstPerson
+ * @return {number[]}
+ */
+var findAllPeople = function(n, meetings, firstPerson) {
+    
+};
